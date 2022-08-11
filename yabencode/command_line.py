66d7c24e6f62d20cdd699504d49567f77bb11505
TheAMM <the.actual.amm@gmail.com>
2022-08-11 18:33:59 +0300

    cli: informative truncate, -T shortcut flag

--- yabencode/command_line.py
+++ yabencode/command_line.py
@@ -6,8 +6,9 @@ import yabencode
 
 parser = argparse.ArgumentParser(description='Bdecode a file/standard input and pretty-print the resulting data')
 parser.add_argument('file', metavar='FILE', help='Input file. Use - for stdin')
-parser.add_argument('-t', '--truncate', metavar='KEY', action='append', help='Truncate values under given key. May be repeated for multiple values')
+parser.add_argument('-t', '--truncate', metavar='KEY', action='append', default=[], help='Truncate values under given key. May be repeated for multiple values')
 parser.add_argument('-r', '--raw', action='store_true', help='Raw keys - do not decode dictionary keys')
+parser.add_argument('-T', '--truncate-pieces', action='store_true', help='Shortcut flag for --truncate pieces')
 parser.add_argument('-V', '--version', action='version', version='%(prog)s {}'.format(yabencode.version), help='Show the version number and exit')
 
 
@@ -27,15 +28,31 @@ class NoStringWrappingPrettyPrinter(pprint.PrettyPrinter):
 
 def _truncate_keys(target_dict, keys):
     '''Replace given keys with <truncated>'''
+    keys = set(keys)
+
     for key, value in target_dict.items():
         if key in keys:
-            target_dict[key] = '<truncated>'
+
+            truncated = None
+            if isinstance(value, bytes):
+                truncated = '<truncated {} bytes>'.format(len(value))
+            elif isinstance(value, str):
+                truncated = '<truncated {}-char string>'.format(len(value))
+            elif isinstance(value, list):
+                truncated = '<truncated {}-item list>'.format(len(value))
+            elif isinstance(value, dict):
+                truncated = '<truncated {}-item dict>'.format(len(value))
+
+            target_dict[key] = truncated or '<truncated>'
+
         elif isinstance(value, dict):
             _truncate_keys(value, keys)
 
 
 def main():
     args = parser.parse_args()
+    if args.truncate_pieces:
+        args.truncate.append('pieces')
 
     if args.file == '-':
         # .buffer for raw bytes
